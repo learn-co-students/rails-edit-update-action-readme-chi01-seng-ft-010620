3120183859223117441255a20474ee20f0fb0bc8
Cesar <cesarmota@ymail.com>
2020-02-03 22:32:38 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  resources :articles , only: [:index,:new,:create,:show]
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch 'articles/:id' ,to:'articles#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+# Prefix Verb  URI Pattern                  Controller#Action
+#     articles GET   /articles(.:format)          articles#index
+#              POST  /articles(.:format)          articles#create
+#  new_article GET   /articles/new(.:format)      articles#new
+# edit_article GET   /articles/:id/edit(.:format) articles#edit
+#              PATCH /articles/:id(.:format)      articles#update
